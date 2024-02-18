8e4a81454d2aa9bb019f00aa9c8209fe7d0839a3
Ahd-3hd <ahdhani0@gmail.com>
2024-02-18 20:15:30 +0300

    feat: get image items endpoint

--- src/controllers/DatasetItem.controller.ts
+++ src/controllers/DatasetItem.controller.ts
@@ -1,15 +1,38 @@
 import { Request, Response, NextFunction } from "express";
 import { DatasetItemModel } from "../models/DatasetItem.model";
 
+export const getDatasetItems = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { workspaceId, projectId, datasetId } = req;
+
+    const datasetItems = await DatasetItemModel.findAll({
+      where: {
+        workspace: workspaceId,
+        project: projectId,
+        dataset: datasetId,
+      },
+    });
+
+    res.status(200).json({
+      data: datasetItems,
+      message: "Dataset items found",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const createDatasetItem = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const { userId, file, workspaceId, projectId, datasetId } = req;
-
-    console.log(file);
+    const { file, workspaceId, projectId, datasetId } = req;
 
     if (!file) throw new Error("File not found");
 
--- src/routers/datasetItem.router.ts
+++ src/routers/datasetItem.router.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
-import { createDatasetItem } from "../controllers/DatasetItem.controller";
+import {
+  createDatasetItem,
+  getDatasetItems,
+} from "../controllers/DatasetItem.controller";
 import { uploadHandler } from "../middlewares/upload.middleware";
 import { namespaceCheck } from "../middlewares/namespaceCheck.middleware";
 import { auth } from "../middlewares/auth.middleware";
 
 const router = Router();
+
+// /api/v1/datasetitem
+router.get(
+  "/:workspaceId/:projectId/:datasetId",
+  auth,
+  namespaceCheck,
+  getDatasetItems
+);
 
 // /api/v1/datasetitem
 router.post(
